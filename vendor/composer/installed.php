56ded1d9ef23bec421b4a779997131cd040fc13f
falcon0920 <hiro_nakaji@icloud.com>
2021-06-19 00:37:41 +0900

    user login

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '16876bc543ecb432404ca597aad6f35fe5283d2a',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '16876bc543ecb432404ca597aad6f35fe5283d2a',
+    ),
+  ),
+);
